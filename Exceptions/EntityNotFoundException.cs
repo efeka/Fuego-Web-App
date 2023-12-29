b56e6b7723f69118ab168309b2e5620e50001081
Efe Karaman <efekaraman@hotmail.com>
2023-12-29 17:23:29 +0300

    Added Exceptions layer with the custom exception EntityNotFoundException

---
+++ Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException() : base($"Could not find Entity.") { }
+        public EntityNotFoundException(int entityId) : base($"Could not find Entity with ID {entityId}.") { }
+    }
+}
