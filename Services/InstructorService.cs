bcbab36b8488fa104d1da387542d4483329caf9b
Efe Karaman <efekaraman@hotmail.com>
2024-01-02 15:07:48 +0300

    Centralized image handling

--- Services/InstructorService.cs
+++ Services/InstructorService.cs
@@ -1,31 +1,34 @@
 ﻿using Data.Repository;
 using Exceptions;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Models;
 using System.Linq.Expressions;
+using Utility;
 
 namespace Services
 {
     public class InstructorService : IGenericService<Instructor>
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ImageHandler _imageHandler;
 
         private readonly string _instructorImagesDir;
         private readonly string _defaultImageFileName;
+        private readonly string _defaultImagePath;
 
-        public InstructorService(IConfiguration config, IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        public InstructorService(IConfiguration config, IUnitOfWork unitOfWork, ImageHandler imageHandler)
         {
             _unitOfWork = unitOfWork;
-            _webHostEnvironment = webHostEnvironment;
+            _imageHandler = imageHandler;
 
             _instructorImagesDir = config["RelativeWWWImagePaths:InstructorImagesDir"]
                 ?? throw new MissingConfigurationException("RelativeWWWImagePaths:InstructorImagesDir");
 
             _defaultImageFileName = config["RelativeWWWImagePaths:DefaultImageFileName"]
                 ?? throw new MissingConfigurationException("RelativeWWWImagePaths:DefaultImageFileName");
+
+            _defaultImagePath = Path.Combine("\\", _instructorImagesDir, _defaultImageFileName);
         }
 
         public async Task<IEnumerable<Instructor>> GetAllAsync(string? includeProperties = null)
@@ -53,21 +56,29 @@ namespace Services
             if (entity.LastPayment == null)
                 entity.LastPayment = DateTime.MinValue;
 
+            string imageUrl = _imageHandler.GetNewOrDefaultImagePath(file, _instructorImagesDir, _defaultImageFileName);
+
             // Insert
             if (entity.Id == 0)
             {
-                AddNewOrDefaultImage(entity, file);
+                entity.ImageUrl = imageUrl;
+                if (file != null)
+                    _imageHandler.SaveImage(file, imageUrl);
+
                 _unitOfWork.Instructor.Add(entity);
                 await _unitOfWork.SaveChangesAsync();
             }
             // Update
             else
             {
-                // If a new image file was provided, delete the old image first
                 if (file != null)
                 {
-                    DeleteImageFile(entity.ImageUrl);
-                    AddNewOrDefaultImage(entity, file);
+                    // Delete old image before creating the new one
+                    _imageHandler.DeleteImageFile(entity.ImageUrl, _defaultImagePath);
+
+                    entity.ImageUrl = imageUrl;
+                    if (file != null)
+                        _imageHandler.SaveImage(file, imageUrl);
                 }
 
                 _unitOfWork.Instructor.Update(entity);
@@ -81,44 +92,9 @@ namespace Services
             if (instructorToBeDeleted == null)
                 throw new EntityNotFoundException(id);
 
-            DeleteImageFile(instructorToBeDeleted.ImageUrl);
+            _imageHandler.DeleteImageFile(instructorToBeDeleted.ImageUrl, _defaultImagePath);
             _unitOfWork.Instructor.Delete(instructorToBeDeleted);
             await _unitOfWork.SaveChangesAsync();
-        }
-
-        private void DeleteImageFile(string imageUrl)
-        {
-            string defaultImagePath = Path.Combine("\\", _instructorImagesDir, _defaultImageFileName);
-            if (imageUrl != defaultImagePath)
-            {
-                var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, imageUrl.TrimStart('\\'));
-                if (System.IO.File.Exists(oldImagePath))
-                    System.IO.File.Delete(oldImagePath);
-            }
-        }
-
-        private void AddNewOrDefaultImage(Instructor entity, IFormFile? file)
-        {
-            string imageFileName;
-            string imagesDir = Path.Combine(_webHostEnvironment.WebRootPath, _instructorImagesDir);
-
-            if (file != null)
-                imageFileName = SaveImage(file, imagesDir);
-            else
-                imageFileName = _defaultImageFileName;
-
-            entity.ImageUrl = Path.Combine("\\", _instructorImagesDir, imageFileName);
-        }
-
-        private string SaveImage(IFormFile file, string imagesDir)
-        {
-            string imageFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-            string imagePath = Path.Combine(imagesDir, imageFileName);
-
-            using var fileStream = new FileStream(imagePath, FileMode.Create);
-            file.CopyTo(fileStream);
-
-            return imageFileName;
         }
     }
 }
---
+++ Utility/ImageHandler.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Utility
+{
+    public class ImageHandler
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageHandler(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        /// <summary>
+        /// Creates a unique name for the given file and returns its path relative to wwwroot.
+        /// If the file is null, returns the relative path for the default image.
+        /// </summary>
+        /// <param name="file">The image file.</param>
+        /// <param name="imagesDir">The path to the images directory relative to wwwroot.</param>
+        /// <param name="defaultImageFileName">The file name of the default image.</param>
+        /// <returns>The path to the new or default image relative to wwwroot</returns>
+        public string GetNewOrDefaultImagePath(IFormFile? file, string imagesDir, string defaultImageFileName)
+        {
+            if (string.IsNullOrEmpty(imagesDir))
+                throw new ArgumentException("Images directory cannot be null or empty.", nameof(imagesDir));
+
+            if (string.IsNullOrEmpty(defaultImageFileName))
+                throw new ArgumentException("Default image file name cannot be null or empty.", nameof(defaultImageFileName));
+
+            string imageFileName = file != null
+                ? Path.GetRandomFileName() + Path.GetExtension(file.FileName)
+                : defaultImageFileName;
+
+            return Path.Combine("\\", FormatPath(imagesDir), FormatPath(imageFileName));
+        }
+
+        /// <summary>
+        /// Deletes the image file located at the specified imagePath, excluding the default image.
+        /// </summary>
+        /// <param name="imagePath">The path to the image file relative to wwwroot.</param>
+        /// <param name="defaultImagePath">The path to the default image file relative to wwwroot (optional).</param>
+        public void DeleteImageFile(string imagePath, string defaultImagePath = "")
+        {
+            try
+            {
+                if (imagePath != defaultImagePath)
+                {
+                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, FormatPath(imagePath));
+                    if (System.IO.File.Exists(oldImagePath))
+                        System.IO.File.Delete(oldImagePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting image file: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Saves the provided image file to the specified imagePath.
+        /// </summary>
+        /// <param name="file">The image file to save.</param>
+        /// <param name="imagePath">The path where the image will be saved relative to wwwroot.</param>
+        /// <exception cref="ArgumentException">Thrown if the imagePath is null or empty.</exception>
+        public void SaveImage(IFormFile file, string imagePath)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(imagePath))
+                    throw new ArgumentException("The imagePath cannot be null or empty.", nameof(imagePath));
+
+                string fullDirPath = Path.Combine(_webHostEnvironment.WebRootPath, FormatPath(imagePath));
+
+                using var fileStream = new FileStream(fullDirPath, FileMode.Create);
+                file.CopyTo(fileStream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving image file: {ex.Message}");
+                throw;
+            }
+        }
+
+        private string FormatPath(string path)
+        {
+            return path.TrimStart('~', '/').Replace("/", Path.DirectorySeparatorChar.ToString()).TrimStart('\\');
+        }
+    }
+}
